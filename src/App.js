7bba199562ac6d34c009e9972ce5359be832a76a
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2023-08-20 00:44:12 -0400

    reduce to workshop starter code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Navigation from './components/Navigation';
+// import Home from './pages/Home';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,8 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div className='navbar'>
-      <Link className='navbar-link' to='/'>Home</Link>
-      <Link className='navbar-link' to='/projects'>Projects</Link>
-      <Link className='navbar-link' to='contact'>Contact</Link>
     </div>
   );
 };
